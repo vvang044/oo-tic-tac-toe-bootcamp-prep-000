7e3752e9c961f2e735f64442635f9bcbd6ec8b6b
Vik Vangala <vvang044@gmail.com>
2019-11-23 02:17:10 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -88,5 +88,16 @@ def turn
       turn
     end
 end  
+
+def play
+  while over? == false
+    turn
+  end
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
   
  end 
