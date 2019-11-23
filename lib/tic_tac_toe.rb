6cc0928de888b0123c99e82c9c48c3174d860e2d
Vik Vangala <vvang044@gmail.com>
2019-11-23 02:14:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -71,16 +71,7 @@ def over?
 end 
 
 def winner
-  WIN_COMBINATIONS.detect do |win_combo|
-    if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
-      return "X"
-    elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
-      return "O"
-    else
-      nil
-    end
-  end
-end
+
 end
   
   
