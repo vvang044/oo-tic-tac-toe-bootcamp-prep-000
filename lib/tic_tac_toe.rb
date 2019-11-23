d9775ca1a6efeb1ceaec59eac4bc46a3022f1519
Vik Vangala <vvang044@gmail.com>
2019-11-23 02:16:09 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -76,6 +76,17 @@ def winner
   end 
 end
   
-  
+def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    char = current_player
+    if valid_move?(index)
+      move(index, char)
+      display_board
+    else
+      turn
+    end
+end  
   
  end 
